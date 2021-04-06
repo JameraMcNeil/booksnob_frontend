964a74f97ce3f355e7b05aaf23190a208e3ea8ac
jamera mcneil <j.mcneil04@gmail.com>
2021-04-05 21:27:58 -0400

    created a component called for the new form

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ class App extends Component {
       books: []
     };
     this.getBooks = this.getBooks.bind(this);
+    this.handleAddBooks = this.handleAddBooks.bind(this);
+    this.handleChange = this.handleChange(this);
+    this.handleSubmit = this.handleSubmit(this);
   }
 
   componentDidMount() {
@@ -30,6 +33,42 @@ class App extends Component {
       err => console.log(err))
   }
 
+  handleAddBooks(book) {
+    const copyBooks = [...this.state.books]
+    copyBooks.unshift(book)
+    this.setState({
+      books: copyBooks
+    })
+  }
+
+  handleChange(event) {
+    console.log(event.target.value)
+    this.setState({ [event.target.id]: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(event)
+
+    fetch(baseURL + '/books/' + book._id, {
+      method: 'PUT',
+      body: JSON.stringify({
+        img: '',
+        description: ''
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res.json())
+      .then(resJson => {
+        const copyBooks = [...this.state.books]
+        const findIndex = this.state.books.findIndex(book => 
+          book._id === resJson._id)
+          copyBooks [findIndex] = resJson
+          this.setState({books: copyBooks})
+      })
+  }
+
   render() {
     return (
       <div>
@@ -37,8 +76,18 @@ class App extends Component {
         <header>
           <h1>Booksnob</h1>
         </header>
+      <br/>
+
+      <form onSubmit = { this.handleSubmit }>
+        <input type = 'text' name = 'img' id = 'img' onChange = {this.handleChange} placeholder = 'Add image' value = {this.state.img} />
+        <label htmlFor = 'img'> Book Cover Image</label>
+        <input type ='text' name = 'description' id = 'description' onChange = { this.handleChange } placeholder = 'Add description' value = { this. state. description } />
+        <label htmlFor = 'description'>Description</label>
+        <input type = 'submit' value = 'Add Book' />
+      </form>
   
         <br/>
+        <h2>Book Recommendations</h2>
         <ul>
           {
             this.state.books.map((book, key) => {
---
+++ src/components/NewForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+
+class NewForm extends Component {
+    render () {
+        return(
+            <h2>New form here</h2>
+        )
+    }
+}
+
+export default NewForm
