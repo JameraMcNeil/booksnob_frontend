47c8474f8d98b8f7a8a8b73c31e82ae9100798d4
jamera mcneil <j.mcneil04@gmail.com>
2021-04-06 13:08:20 -0400

    added list of youtubers with links to the account

--- src/components/BooktubeList.jsx
+++ src/components/BooktubeList.jsx
@@ -4,7 +4,19 @@ class BooktubeList extends Component {
     render() {
         return(
             <div>
-                <h2>Booktube List Goes Here</h2>
+                <h2>Booktubers To Follow:</h2>
+                <br/>
+                <p><a href = 'https://www.youtube.com/channel/UCpC_4ZZjQqjXnxYGy35CFzw' target = '_blank'>Left on Read</a></p>
+                <br/>
+                <p><a href = 'https://www.youtube.com/user/PeruseProject' target = '_blank'>Peruse Project</a></p>
+                <br/>
+                <p><a href = 'https://www.youtube.com/user/TheNewGirl007' target = '_blank'>Alexandria The Great</a></p>
+                <br/>
+                <p><a href = 'https://www.youtube.com/user/sunbeamsjess' target = '_blank'>Sunbeams Jess</a></p>
+                <br/>
+                <p><a href = 'https://www.youtube.com/channel/UC9wGRQa-JiXIbwjGDl5SSgw' target = '_blank'>By Amber Burns</a></p>
+                <br/>
+                <p><a href = 'https://www.youtube.com/channel/UCMlBZSkLgZFxotyzSIJNfBQ' target = '_blank'>Pretty x Bookish</a></p>
             </div>
         )
     }
