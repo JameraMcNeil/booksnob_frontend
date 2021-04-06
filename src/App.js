abbf64524d34be78f057a0d9034cca84ad5bb701
jamera mcneil <j.mcneil04@gmail.com>
2021-04-05 20:35:07 -0400

    renders seed data and list of books onto page. For some reason the image isn't displaying though

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let baseURL = ''
+
+if(process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3003'
+} else {
+  baseURL = 'your heroku backend url here'
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: []
+    };
+    this.getBooks = this.getBooks.bind(this);
+  }
+
+  componentDidMount() {
+    this.getBooks()
+  };
+
+  getBooks() {
+    fetch(baseURL + '/books')
+    .then(data => {
+      return data.json()},
+      err => console.log(err))
+      .then(json => this.setState({books:json}),
+      err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+      
+        <header>
+          <h1>Booksnob</h1>
+        </header>
+  
+        <br/>
+        <ul>
+          {
+            this.state.books.map((book, key) => {
+              return <li key = {key}> { book.img } | { book.description }</li>
+            })
+          }
+        </ul>
+
+      </div>
+    )
+  }
 }
 
 export default App;
