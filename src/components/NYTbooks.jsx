7328a5d49a132da9fe4dce5699cb5e18fe1e0dd3
jamera mcneil <j.mcneil04@gmail.com>
2021-04-07 11:20:16 -0400

    successfully accessed data in NYT api

--- src/components/NYTbooks.jsx
+++ src/components/NYTbooks.jsx
@@ -1,9 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import env from 'react-dotenv'
+
+
 
 class NYTbooks extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            books: []
+        }
+        this.getBestsellers = this.getBestsellers.bind(this)
+    }
+
+
+    getBestsellers() {
+        fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_BOOKS_API_KEY}`)
+        .then(res => {
+            return res.json()},
+            err => console.log(err))
+        .then((json) => this.setState({books: json.results.books}), 
+        err => console.log(err))
+    }
+
+
     render() {
+        // this.getBestsellers()
+        // console.log(process.env.REACT_APP_BOOKS_API_KEY)
         return(
-            <div>NYT Book list goes here</div>
+            <div>
+                <h2>New York Times Bestsellers List</h2>
+            <button onClick={this.getBestsellers}>TAP</button>
+            
+
+            </div>
+            
         )
     }
 }
