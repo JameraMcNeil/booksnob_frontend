765312ea9fefc664e8ea0a3ff5fe5905e0acfb02
jamera mcneil <j.mcneil04@gmail.com>
2021-04-09 13:07:27 -0400

    added some styling to booktuber list

--- src/components/BooktubeList.jsx
+++ src/components/BooktubeList.jsx
@@ -4,19 +4,31 @@ class BooktubeList extends Component {
     render() {
         return(
             <div>
-                <h2>Booktubers To Follow:</h2>
-                <br/>
-                <p><a href = 'https://www.youtube.com/channel/UCpC_4ZZjQqjXnxYGy35CFzw' target = '_blank' rel='noreferrer'>Left on Read</a></p>
-                <br/>
-                <p><a href = 'https://www.youtube.com/user/PeruseProject' target = '_blank' rel='noreferrer'>Peruse Project</a></p>
-                <br/>
-                <p><a href = 'https://www.youtube.com/user/TheNewGirl007' target = '_blank' rel='noreferrer'>Alexandria The Great</a></p>
-                <br/>
-                <p><a href = 'https://www.youtube.com/user/sunbeamsjess' target = '_blank' rel='noreferrer'>Sunbeams Jess</a></p>
-                <br/>
-                <p><a href = 'https://www.youtube.com/channel/UC9wGRQa-JiXIbwjGDl5SSgw' target = '_blank' rel='noreferrer'>By Amber Burns</a></p>
-                <br/>
-                <p><a href = 'https://www.youtube.com/channel/UCMlBZSkLgZFxotyzSIJNfBQ' target = '_blank' rel='noreferrer'>Pretty x Bookish</a></p>
+                <section>
+                    <div className = 'columns'>
+                        <div className = 'column is-narrow'>
+                            <div className = 'box'>
+                                <p className = 'title is-4'>Booktubers to Follow:</p>
+                                <br/>
+                                <p className = 'subtitle'><a href = 'https://www.youtube.com/channel/UCpC_4ZZjQqjXnxYGy35CFzw' target = '_blank' rel='noreferrer'>Left on Read</a></p>
+                                
+                                <p className = 'subtitle'><a href = 'https://www.youtube.com/user/PeruseProject' target = '_blank' rel='noreferrer'>Peruse Project</a></p>
+                                
+                                <p className = 'subtitle'><a href = 'https://www.youtube.com/user/TheNewGirl007' target = '_blank' rel='noreferrer'>Alexandria The Great</a></p>
+                                
+                                <p className = 'subtitle'><a href = 'https://www.youtube.com/user/sunbeamsjess' target = '_blank' rel='noreferrer'>Sunbeams Jess</a></p>
+                                
+                                <p className = 'subtitle'><a href = 'https://www.youtube.com/channel/UC9wGRQa-JiXIbwjGDl5SSgw' target = '_blank' rel='noreferrer'>By Amber Burns</a></p>
+                                
+                                <p className = 'subtitle'><a href = 'https://www.youtube.com/channel/UCMlBZSkLgZFxotyzSIJNfBQ' target = '_blank' rel='noreferrer'>Pretty x Bookish</a></p>
+                            </div>
+                        
+                        </div>
+                    </div>
+                    <br/>
+                    
+                </section>
+                
             </div>
         )
     }
